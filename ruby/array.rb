4c0b2d8a51375f56d2d4e29f68c23dfd5ac2dcd7
EC2 Default User <ec2-user@ip-172-31-49-146.ec2.internal>
2021-07-02 14:11:20 +0000

    ruby5

---
+++ ruby/array.rb
@@ -0,0 +1,4 @@
+names = ["Git" , "HTML" , "CSS"]
+puts names
+puts names[1]
+puts names[0 ,2]
---
+++ ruby/weight.rb
@@ -0,0 +1,4 @@
+name = "まる太郎"
+weight = 50
+puts name + "さんの体重は" + weight.to_s + "Kgです"
+puts "#{name}さんの体重は#{weight}Kgです"
