2862ee003024e62b0752a47816a81f69cfa675ac
EC2 Default User <ec2-user@ip-172-31-49-146.ec2.internal>
2021-07-05 07:52:06 +0000

    ruby start 5

---
+++ ruby/hand.rb
@@ -0,0 +1,12 @@
+hand = "グー"
+ if hand == "グー" #handはグーである
+   puts "出した手はグーです"
+  end
+  
+if hand != "チョキ" #handはチョキではない
+  puts "出した手はチョキです"
+end
+
+if (hand == "グー") || (hand == "パー") #(handはグーである)または(handはパーである)
+  puts "出した手はグーまたはパーです"
+end
