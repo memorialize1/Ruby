32d7b9c13b484a7655f40a49ad1bcde9214e4a46
EC2 Default User <ec2-user@ip-172-31-49-146.ec2.internal>
2021-07-05 12:15:40 +0000

    ruby finish6

---
+++ ruby/apple.rb
@@ -0,0 +1,10 @@
+apple = "Nagano" , "Aomori"
+
+if apple == "Aomori" #もしappleがAomoriなら
+  puts "このリンゴは青森県産です"
+  elsif apple == "Nagano" #またはappleがNaganoなら
+    puts "このリンゴは青森県産ではなく、長野県産です"
+  else #上記の条件外
+    puts "このリンゴは青森県産でも長野県産でもありません"
+  end
+  
---
+++ ruby/lesson6.rb
@@ -0,0 +1,10 @@
+total_price = 7500
+
+if total_price > 100
+  puts "まだお金あるわよ。"
+  elsif total_price < 100
+    puts "あなたお金足りないわよ。"
+  elsif total_price == 100
+    puts "所持金ちょうどね。もうあなたにお金はないわ。"
+  end
+  
